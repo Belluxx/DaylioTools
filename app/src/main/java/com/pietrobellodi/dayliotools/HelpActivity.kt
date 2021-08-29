5de7e5ac6fefc556e064915139f92783e65cfa42
Pietro Bellodi <78388441+Belluxx@users.noreply.github.com>
2021-08-29 19:12:16 +0200

    Add help page and update dependencies

---
+++ app/src/main/java/com/pietrobellodi/dayliotools/HelpActivity.kt
@@ -0,0 +1,11 @@
+package com.pietrobellodi.dayliotools
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HelpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_help)
+    }
+}
